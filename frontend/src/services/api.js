23b29baeb8e1f6b80ade3ccc0c7f158a7a30d594
harikrishnan1906 <harikrishnan.k1906@gmail.com>
2026-05-10 07:53:20 +0530

    frontend API changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://oceanfresh-backend.onrender.com",
+  baseURL: "https://oceanfresh-backend.onrender.com/api",
   withCredentials: true,
 });
 
